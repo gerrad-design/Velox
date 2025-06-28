5d4ba1e37dcc3f8c1bea9790cbf629700d518aa7
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-06-28 10:45:49 +0300

    added navbar to home page

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<App/>} />
-        <Route path="/" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
