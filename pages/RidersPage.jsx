5baf554d1f420294711b0dfe88e8c9fdf8b7bf0a
Eugene <eugene.chege@student.moringaschool.com>
2025-06-30 02:02:53 +0300

    made changes ridepages

--- pages/RidersPage.jsx
+++ pages/RidersPage.jsx
@@ -4,14 +4,16 @@ import RideStatus from "../components/RideStatus";
 import Navbar from "../components/Navbar";
 
 export default function RidersPage() {
-  
+  const [rideData, setRideData] = useState(null); // shared state
 
   return (
-    <div className="bg-white text-black dark:bg-gray-200">
-        <Navbar/>
-        <BookRide/>
-        <ClientDashboard/>
-        <RideStatus/>
+    <div className="bg-white text-black dark:bg-gray-200 min-h-screen">
+      <Navbar />
+      {!rideData ? (
+        <BookRide setRideData={setRideData} />
+      ) : (
+        <RideStatus rideData={rideData} />
+      )}
     </div>
   );
 }
