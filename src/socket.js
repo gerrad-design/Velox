bb6b29c13f0c09f9fe3d221e4649c9d89332f2d2
Eugene <eugene.chege@student.moringaschool.com>
2025-06-29 23:26:35 +0300

    socket

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("", {
+const socket = io("https://08cc-102-217-167-34.ngrok-free.app", {
   transports: ["websocket", "polling"],
 });
 
