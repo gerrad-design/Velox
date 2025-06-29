008d9a2c54f278627c1aa8d809c7b0faecee5618
Eugene <eugene.chege@student.moringaschool.com>
2025-06-30 02:59:38 +0300

    made changes on main

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<App/>} />
-        <Route path="/RidersPage" element={<App/>} />
+        <Route path="/RidersPage" element={<RidersPage/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
