e6352c8df747e3883dcac0f7b7b082f6d8a6d8ac
Eugene <eugene.chege@student.moringaschool.com>
2025-06-29 23:42:58 +0300

    routed riderspage

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import HomePage from '../pages/HomePage.jsx';
+import RidersPage from '../pages/RidersPage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<App/>} />
+        <Route path="/RidersPage" element={<App/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
